9c003c7ebc8add26fed998ee4554852988fbb1e3
SergeDot <srg@dr.com>
2024-04-06 21:50:33 -0400

    added clearer error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,7 +4,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   };
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.name === 'ValidationError') {
+    const errorMessage = Object.values(err.errors).map(val => val.message);
+    return res.status(err.statusCode).json({ msg: errorMessage.join(', ') })
+  };
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err.name, msg: err.message })
 };
 
 export default errorHandlerMiddleware;

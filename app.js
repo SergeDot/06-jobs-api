bb34f7493ffa1f1ead516f401cb0304becc69e44
SergeDot <srg@dr.com>
2024-04-07 00:51:47 -0400

    testing no POST fix

--- app.js
+++ app.js
@@ -29,14 +29,18 @@ app.use(rateLimit({
 }));
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/food-items', authenticateUser, foodItemsRouter);
 app.get('/', (req, res) => {
-  res.send('food-items api');
+  res.send('Calorie Tracker API');
+});
+
+app.get('/api/v1/auth/login', (req, res) => {
+  res.send('API V1 Auth Test');
 });
 
 app.use(notFoundMiddleware);
